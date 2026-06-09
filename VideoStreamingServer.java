d406070ab47dfb2707f161f60781cc4788f828fa
Rane <gr99307@win.edag.de>
2026-06-09 17:20:40 +0530

    Updated Video files

--- VideoStreamingServer.java
+++ VideoStreamingServer.java
@@ -44,13 +44,13 @@ public class VideoStreamingServer {
  
    public static void main(String[] args) throws IOException {
  
-       Path frontVideo = Paths.get("right (1).mov");
+       Path frontVideo = Paths.get("right (1).mp4");
  
-       Path rearVideo  = Paths.get("rear (1).mov");
+       Path rearVideo  = Paths.get("rear (1).mp4");
  
-       Path sideVideo  = Paths.get("left (1).mov");
+       Path sideVideo  = Paths.get("left (1).mp4");
  
-       Path backVideo  = Paths.get("Front.mp4");
+       Path backVideo  = Paths.get("Front (1).mp4");
  
        Path[] videos = { frontVideo, rearVideo, sideVideo, backVideo };
  
